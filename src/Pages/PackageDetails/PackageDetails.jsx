4271b3b2027f6560aa93f82f360fda9343aa9f77
imranhasan2 <imranhasan030303@gmail.com>
2024-06-10 01:14:27 +0600

    added tour details

--- src/Pages/PackageDetails/PackageDetails.jsx
+++ src/Pages/PackageDetails/PackageDetails.jsx
@@ -9,13 +9,28 @@ const PackageDetails = () => {
     console.log(details)
     return (
         <div>
-            <div className="grid grid-cols-1 lg:grid-cols-2 md:grid-cols-2 gap-5 ">
-                {details?.images?.map(img =>
-                    <div key={img._id}>
+            <div className="grid grid-cols-1 lg:grid-cols-2 md:grid-cols-2 gap-5 mb-5">
+                {details?.images?.map((img,idx) =>
+                    <div key={idx}>
 
                         <img src={img} alt="" />
                     </div>)}
             </div>
+            <h2 className="text-2xl font-bold">GENERAL INFORMATION</h2>
+            <p className="mb-10">{details?.description}</p>
+            <h2 className="text-2xl font-medium">TOUR ITINERARY</h2>
+             {details?.tourPlan?.map((item, index) => (
+                <div key={index} className="collapse collapse-plus bg-blue-500 mb-3">
+                    <input type="radio" name="my-accordion-3" defaultChecked={index === 0} />
+                    <div className="collapse-title text-xl font-medium">
+                        {`0${index + 8}:00 HRS`}
+                    </div>
+                    <div className="collapse-content">
+                        <p>{item}</p>
+                    </div>
+                </div>
+            ))}
+           
         </div>
     );
 };
